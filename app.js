acbfac55331f6be7157e0dda15cdd8dbfb3194aa
Ollie Husband <olliehusband@me.com>
2018-01-16 21:22:52 +0000

    missing files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 8080;
 const shell = require('shelljs');
 const COMMANDS = require("./commands");
 const bodyParser = require('body-parser');
