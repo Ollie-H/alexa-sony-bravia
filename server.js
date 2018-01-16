0b898e02339cd280816f8a9f9bbc5a5a5b4752ed
Ollie Husband <olliehusband@me.com>
2018-01-16 21:15:00 +0000

    changed npm start pointer

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 8000;
-const shell = require('shelljs');
-const COMMANDS = require("./commands");
-const bodyParser = require('body-parser');
-const TV_IP_ADDRESS = process.env.TV_IP_ADDRESS;
+/**
+ * index.js
+ * @author Ollie Husband
+ * @description Entry point for application
+ */
 
-
-app.use(bodyParser.json({ type: 'application/json' }))
-
-app.post('/command', (req, res) => {
-
-	const { command = "" } = req.body;
-
-	const action = COMMANDS.find(action => action.name.toLowerCase() === command.toLowerCase());
-
-	if (!action || !action.value) {
-		res.send({ error: "Action not found" });
-	}
-
-	console.log(`./sh/send_command.sh ${TV_IP_ADDRESS} ${action.value}`);
-	shell.exec(`./sh/send_command.sh ${TV_IP_ADDRESS} ${action.value}`, (code, stdout, stderr) => res.send(({
-		code, stdout, stderr
-	})))
-});
-
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+// ES6 transpiler
+require("babel-polyfill");
+require("babel-register");
+require("./app")
